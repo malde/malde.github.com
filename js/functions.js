7579d569d3fd22726cd7f0e6dd2e15eb133c7351
Malte Klemke <m.klemke@monday-consulting.com>
2013-01-09 15:43:17 +0100

    draws link on canvas, removes old resources

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
-    var datum = new Date();
-    var zeit = datum.getHours();
-    
-    if(zeit > 18 || zeit < 7) {
-        $('html').addClass('night');
-        $('body').addClass('night');
-    }
+
 });
