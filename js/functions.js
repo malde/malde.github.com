a104c7acf83d3f95ef7765d97bfa3458c417ab05
Malte Klemke <m.klemke@monday-consulting.com>
2013-01-12 13:35:28 +0100

    removes margins, changes background, reintroduces background depending on daytime

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+	var datum = new Date();
+    var zeit = datum.getHours();
+    
+    if(zeit > 18 || zeit < 7) {
+ 		nightfall();
+    }
 });
+
+var nightfall = function() {
+	$('body').addClass('night');
+}
