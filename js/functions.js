f71808dbd653ad7a3037d16181a5fed564cb739f
Malte Klemke <m.klemke@monday-consulting.com>
2013-01-17 15:38:37 +0100

    improves colors of rainbow and makes fade much more subtle

--- js/functions.js
+++ js/functions.js
@@ -2,14 +2,9 @@ $(document).ready(function(){
 	rainbow();	
 });
 
-// Red (web color) (Hex: #FF0000) (RGB: 255, 0, 0)
-// Orange (color wheel Orange) (Hex: #FF7F00) (RGB: 255, 127, 0)
-// Yellow (web color) (Hex: #FFFF00) (RGB: 255, 255, 0)
-// Green (X11) (Electric Green) (HTML/CSS “Lime”) (Color wheel green) (Hex: #00FF00) (RGB: 0, 255, 0)
-// Blue (web color) (Hex: #0000FF) (RGB: 0, 0, 255)
-// Indigo (Electric Indigo) (Hex: #6600FF) (RGB: 111, 0, 255)
-// Violet (Electric Violet) (Hex: #8B00FF) (RGB: 143, 0, 255)
-var spectrum = [ "#FF0000", "#FF7F00", "#FFFF00", "#00FF00", "#0000FF", "#6600FF", "#8B00FF" ];
+var spectrum = ["#80F31F", "#A5DE0B", "#C7C101", "#E39E03", "#F6780F", "#FE5326", "#FB3244", "#ED1868", 
+			 	"#D5078E", "#B601B3", "#9106D3", "#6B16EC", "#472FFA", "#2850FE", "#1175F7", "#039BE5", 
+				"#01BECA", "#0ADCA8", "#1DF283", "#3AFD5D", "#5CFD3A"]
 var index = 0;
 
 var rainbow = function() {
