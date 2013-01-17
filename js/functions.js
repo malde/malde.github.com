19cdb2fccda8ffba104383504c478aff69b82540
Malte Klemke <m.klemke@monday-consulting.com>
2013-01-17 15:10:01 +0100

    double rainbow all the way

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,20 @@
-$(document).ready(function(){
-	var datum = new Date();
-    var zeit = datum.getHours();
-    
-    if(zeit > 18 || zeit < 7) {
- 		nightfall();
-    }
+$(document).ready(function(){	
+	rainbow();	
 });
 
-var nightfall = function() {
-	$('body').addClass('night');
+// Red (web color) (Hex: #FF0000) (RGB: 255, 0, 0)
+// Orange (color wheel Orange) (Hex: #FF7F00) (RGB: 255, 127, 0)
+// Yellow (web color) (Hex: #FFFF00) (RGB: 255, 255, 0)
+// Green (X11) (Electric Green) (HTML/CSS “Lime”) (Color wheel green) (Hex: #00FF00) (RGB: 0, 255, 0)
+// Blue (web color) (Hex: #0000FF) (RGB: 0, 0, 255)
+// Indigo (Electric Indigo) (Hex: #6600FF) (RGB: 111, 0, 255)
+// Violet (Electric Violet) (Hex: #8B00FF) (RGB: 143, 0, 255)
+var spectrum = [ "#FF0000", "#FF7F00", "#FFFF00", "#00FF00", "#0000FF", "#6600FF", "#8B00FF" ];
+var index = 0;
+
+var rainbow = function() {
+	$('body').css('background', spectrum[index]);
+    index = ++index % spectrum.length;
+
+	setTimeout(rainbow, 2000);
 }
